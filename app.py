d13c164d3051f00d69957826b6e288fce9755197
mmchugh <michael.mchugh@urbanairship.com>
2016-11-06 22:39:25 -0800

    add multiple octaves

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 import generate
 
@@ -20,6 +20,16 @@ def js(path):
 
 @app.route('/data/<style>/')
 def output(style):
-    return json.dumps(generate.generate(style=style))
+    if style == 'random':
+        value_func = generate.random
+    elif style == 'sine':
+        value_func = generate.simple_sine
+    elif style == 'octaves':
+        # get octaves
+        octaves = int(request.args.get('octaves', 4))
+        amplitude = int(request.args.get('amplitude', 5))
+        value_func = generate.multiple_octaves(octaves, amplitude)
+
+    return json.dumps(generate.generate(value_func))
 
 app.run('127.0.0.1', 8088, debug=True)
--- generate.py
+++ generate.py
@@ -2,6 +2,9 @@ import json
 import math
 import random
 
+WIDTH = 20
+HEIGHT = 20
+
 
 def all_random(x, y):
     return random.random() * 20.0
@@ -11,32 +14,55 @@ def simple_sine(x, y):
     return math.sin(x) * 5.0 + math.sin(y) * 5.0
 
 
-STYLES = {
-    'random': all_random,
-    'sine': simple_sine,
-}
+def multiple_octaves(octaves, start_amplitude):
+    parameters = []
+    for i in range(octaves):
+        parameters.append({
+            'offset': random.random() * 2 * math.pi,
+            'frequency': 2**i,
+            'amplitude': start_amplitude / float(i+1),
+        })
+
+    print parameters
+
+    def noise(x, y):
+        value = 0
+        for p in parameters:
+            x_part = math.sin(
+                (x / float(WIDTH))
+                * p['frequency']
+                * 2 * math.pi
+                + p['offset']
+            )
+            y_part = math.sin(
+                (y / float(HEIGHT))
+                * p['frequency']
+                * 2 * math.pi
+                + (x_part % (2 * math.pi))
+            )
+            value += y_part * p['amplitude']
+
+        return value
+
+    return noise
 
 
-def value(x, y, style):
-    return STYLES[style](x, y)
-
-
-def generate(width=20, height=20, style='sine'):
+def generate(value):
     values = []
 
-    for x in range(width):
-        for y in range(height):
-            values.append(value(x, y, style))
+    for x in range(WIDTH):
+        for y in range(WIDTH):
+            values.append(value(x, y))
 
     return {
-        'width': width,
-        'height': height,
+        'width': WIDTH,
+        'height': HEIGHT,
         'values': values,
     }
 
 
 if __name__ == '__main__':
-    data = generate()
+    data = generate(simple_sine)
 
     with open('output.json', 'w') as out:
         json.dump(data, out)
